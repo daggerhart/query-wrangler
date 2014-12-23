310b3ade63c0f2a73531b16704e6cab12254fbcc
Jonathan Daggerhart <jonathan@daggerhart.com>
2014-12-23 00:52:00 -0500

    Cleaning up editor themes and removing old jQuery ui theme files

--- admin/editors/picnic/picnic.js
+++ admin/editors/picnic/picnic.js
@@ -1,74 +1,82 @@
 (function( $ ){
 
+  QWPicnic = {
+    // prevent queuing toggles
+    is_toggling: false,
 
-  // prevent queuing toggles
-  var qw_ui_is_toggling = false;
+    /**
+     *
+     */
+    init: function(){
+      // sortable item lists
+      QueryWrangler.sortables.init('.qw-handler-items', '.qw-handler-item');
 
-  /**
-   * Toggle an element if no other element is toggling
-   *
-   * @param element - the element to toggle
-   */
-  function _toggle( element ){
-    if (!qw_ui_is_toggling) {
-      qw_ui_is_toggling = true;
+      // freshen the ui js
+      QWPicnic.refresh_ui();
 
-      $(element).toggle( 300, function(){
-        qw_ui_is_toggling = false;
-      });
-    }
-  }
-
-  /**
-   * Refresh the UI components on the page
-   * @private
-   */
-  function _refresh_ui(){
-    $('.qw-add-new-items').hide();
-    $('textarea.qw-field-textarea').addClass('blurred');
-
-    QueryWrangler.jsTitles.refresh();
-    QueryWrangler.optionGroups.refresh();
-    QueryWrangler.sortables.refresh();
-  }
-
-
-  // init
-  $(document).ready(function(){
-    // sortable item lists
-    QueryWrangler.sortables.init('.qw-handler-items', '.qw-handler-item');
-
-    // freshen the ui js
-    _refresh_ui();
-
-    // open and close the 'add new' checkbox groups
-    $('.qw-add-item').click(function(){
-      _toggle( $(this).parent().next('.qw-add-new-items') );
-    });
-
-    // open and close single handler items
-    $('.qw-handler-items').on('click', '.qw-setting-header', function(){
-      $(this).parent().find('.group').toggle();
-    });
-
-    // delegated events
-    $('#qw-edit-query-form')
-      // focus and blur for textareas
-      .on('focus', 'textarea.qw-field-textarea', function(){
-        $(this).removeClass('blurred');
-      })
-      .on('blur', 'textarea.qw-field-textarea', function(){
-        $(this).addClass('blurred');
-      })
-      // remove buttons
-      .on('click', '.qw-remove', function(){
-        $(this).closest('.qw-handler-item').remove();
-        QueryWrangler.sortables.updateItemWeights();
+      // open and close the 'add new' checkbox groups
+      $('.qw-add-item').click(function(){
+        QWPicnic.toggle( $(this).parent().next('.qw-add-new-items') );
       });
 
-    // get new handler items forms
-    $('.qw-add-items-submit').click(function(){
-      var handler = $(this).attr('title');
+      // open and close single handler items
+      $('.qw-handler-items').on('click', '.qw-setting-header', function(){
+        $(this).parent().find('.group').toggle();
+      });
+
+      // delegated events
+      $('#qw-edit-query-form')
+        // focus and blur for textareas
+        .on('focus', 'textarea.qw-field-textarea', function(){
+          $(this).removeClass('blurred');
+        })
+        .on('blur', 'textarea.qw-field-textarea', function(){
+          $(this).addClass('blurred');
+        })
+        // remove buttons
+        .on('click', '.qw-remove', function(){
+          $(this).closest('.qw-handler-item').remove();
+          QueryWrangler.sortables.updateItemWeights();
+        });
+
+      // get new handler items forms
+      $('.qw-add-items-submit').click( QWPicnic.addHandlerItems );
+    },
+
+    /**
+     * Toggle an element if no other element is toggling
+     *
+     * @param element - the element to toggle
+     */
+    toggle: function ( element ){
+      if (!QWPicnic.is_toggling) {
+        QWPicnic.is_toggling = true;
+
+        $(element).toggle( 300, function(){
+          QWPicnic.is_toggling = false;
+        });
+      }
+    },
+
+    /**
+     * Refresh the UI components on the page
+     * @private
+     */
+    refresh_ui: function(){
+      $('.qw-add-new-items').hide();
+      $('textarea.qw-field-textarea').addClass('blurred');
+
+      QueryWrangler.jsTitles.refresh();
+      QueryWrangler.optionGroups.refresh();
+      QueryWrangler.sortables.refresh();
+    },
+
+    /**
+     * Loop through handler item checkboxes,
+     *   get the template for the item, and append to the list
+     */
+    addHandlerItems: function(){
+      var handler = $(this).data('handler-type');
       var checkedboxes = $(this).closest('.qw-add-new-items').find('input[type=checkbox]:checked');
 
       $.each( checkedboxes, function( index, box ){
@@ -90,9 +98,15 @@
         });
       });
 
-      _toggle( $(this).closest('.qw-add-new-items') );
-      _refresh_ui();
-    });
+      QWPicnic.toggle( $(this).closest('.qw-add-new-items') );
+      QWPicnic.refresh_ui();
+    }
+  };
+
+
+  // init
+  $(document).ready(function(){
+    QWPicnic.init();
   });
 
 })(jQuery);
